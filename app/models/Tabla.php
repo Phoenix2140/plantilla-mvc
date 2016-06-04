68e5e4a1e393a905119d6036250e9cc7c159b585
Héctor Álvarez <phoenix@altro>
2016-06-04 11:41:17 -0400

    Agregadas dos funciones nuevasal modelo Tabla

--- app/models/Tabla.php
+++ app/models/Tabla.php
@@ -24,5 +24,41 @@
 
 			return $this->db->resultSet();
 		}
+
+		/**
+		 * Función que utiliza bind y execute para insertar un
+		 * nuevo registro a la base de datos
+		 */
+		public function createTabla($nombre){
+			$this->db->query("INSERT INTO tabla (nombre)
+				VALUES (:nombre)");
+
+			$this->db->bind(':nombre', $nombre);
+
+			$this->db->execute();
+
+		}
+
+		/**
+		 * Función que actualiza el nombre en la tabla por
+		 * su id utilizando bind y execute de la clase Database
+		 *
+		 */
+		public function updateTabla($id, $nombre){
+			$this->db->query("UPDATE tabla SET nombre=:nombre
+				WHERE id=:id");
+
+			/**
+			 * La función Bind de la clase Database se 
+			 * encarga de vincular una variable al signo del
+			 * sql, luego en execute se evalua todo y se realiza
+			 * la query ya preparada.
+			 */
+			$this->db->bind(':id', $id);
+			$this->db->bind(':nombre', $nombre);
+
+			$this->db->execute();
+
+		}
 	}
  ?>
